ca73add633b1783ffb3141fa72edd2a9cbef3bad
abc <abc@example.com>
2020-07-29 07:53:05 +0530

    Updates

---
+++ precache-manifest.7836e60a955d90f8119870c4a1df6ddd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f8a4339db7192e402121566ce408b3",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "a35b381a50b09eb88004",
+    "url": "/first-react-project/static/css/main.91373937.chunk.css"
+  },
+  {
+    "revision": "d704a949688d0ee2b42d",
+    "url": "/first-react-project/static/js/2.d404129b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-project/static/js/2.d404129b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35b381a50b09eb88004",
+    "url": "/first-react-project/static/js/main.9b1a46fa.chunk.js"
+  },
+  {
+    "revision": "7a2eca2eaa8c9b9934e7",
+    "url": "/first-react-project/static/js/runtime-main.89d1da5e.js"
+  }
+]);
